b3aa2a01028ee63c49aa486d3306a5ff593d40fb
kirigaya211 <kirigayarz@gmail.com>
2026-08-08 19:45:06 +0800

    feat: Implement ItemController with basic structure and routing

---
+++ Alterian_POS/Alterian_POS/controller/ItemController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Alterian_POS.controller
+{
+    [Route("/Item")]
+    [Controller]
+   public class ItemController: ControllerBase
+    {
+        public ItemController(){}
+
+        
+    }
+
+}
